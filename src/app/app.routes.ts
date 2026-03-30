d4a355db0d9b1457d5eb0f1a74abf41ec7bb8b7b
elakkiya <elakkiya2999@gmail.com>
2026-03-30 13:22:47 +0530

    initial ui for activity and dashboard page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Activity } from './pages/activity/activity';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Dashboard },
+    { path: 'dashboard', component: Dashboard },
+    { path: 'activity', component: Activity },
+];
